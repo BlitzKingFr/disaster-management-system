f4589e9882d4c487b384244d85bdac9f79d1874d
Jevis Kafle <jeviskafle@gmail.com>
2026-01-12 11:35:33 +0545

    Components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
-    <div>Bombaclaut</div>
+    <div>
+      <Navbar />
+      <Header />
+      <Footer />
+    </div>
   );
 }
